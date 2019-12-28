0328427c8d2aa652cd3b9f40314eab13efb5ab6b
RicardoKd <richardo.kadyan@gmail.com>
2019-12-28 16:52:31 +0200

    Style added

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const input = document.getElementById("textInput");
 // Button
 const highlightVowelsButton = document.getElementById("highlightVowelsButton");
 
-highlightVowelsButton.addEventListener('click', function() {
-
+highlightVowelsButton.addEventListener("click", function() {
   // Clear div
   divWithText.innerText = "";
 
   // Text from input
-  const inputText = input.value;  
+  const inputText = input.value;
   // each word array
   const eachLetterFromText = inputText.split("");
 
   for (let i = 0; i < eachLetterFromText.length; i++) {
-
     // Letter
     const letter = eachLetterFromText[i];
 
@@ -26,10 +24,21 @@ highlightVowelsButton.addEventListener('click', function() {
     // An <span> gets the value from an array as the 'for' goes on
     newSpan.innerHTML = eachLetterFromText[i];
 
-    if (letter === "а" || letter === "у" || letter === "о" || letter === "ы" || letter === "и" || letter === "э" || letter === "я" || letter === "ю" || letter === "ё" || letter === "е") {
+    if (
+      letter === "а" ||
+      letter === "у" ||
+      letter === "о" ||
+      letter === "ы" ||
+      letter === "и" ||
+      letter === "э" ||
+      letter === "я" ||
+      letter === "ю" ||
+      letter === "ё" ||
+      letter === "е"
+    ) {
       newSpan.classList.add("highlight");
     }
-  
+
     // Making the <span> element show up
     divWithText.appendChild(newSpan);
   }
