07e4d7bb113bf92fe0683cb1f1f7e58ac78d1b41
RicardoKd <richardo.kadyan@gmail.com>
2019-12-29 07:56:05 +0200

    Fixed bugs with capital letters and made text more readable

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const divWithText = document.getElementById("highlightedText");
 const input = document.getElementById("textInput");
 // Button
 const highlightVowelsButton = document.getElementById("highlightVowelsButton");
-
+// Pasting the text from the input to the div below and highlighting vowels
 highlightVowelsButton.addEventListener("click", function() {
   // Clear div
   divWithText.innerText = "";
-
   // Text from input
   const inputText = input.value;
   // each word array
@@ -17,13 +16,11 @@ highlightVowelsButton.addEventListener("click", function() {
   for (let i = 0; i < eachLetterFromText.length; i++) {
     // Letter
     const letter = eachLetterFromText[i];
-
     // Create <span> element
     let newSpan = document.createElement("span");
-
     // An <span> gets the value from an array as the 'for' goes on
     newSpan.innerHTML = eachLetterFromText[i];
-
+    // Cheking for vowels
     if (
       letter === "а" ||
       letter === "у" ||
@@ -34,14 +31,23 @@ highlightVowelsButton.addEventListener("click", function() {
       letter === "я" ||
       letter === "ю" ||
       letter === "ё" ||
-      letter === "е"
+      letter === "е" ||
+      letter === "А" ||
+      letter === "У" ||
+      letter === "О" ||
+      letter === "И" ||
+      letter === "Э" ||
+      letter === "Я" ||
+      letter === "Ю" ||
+      letter === "Ё" ||
+      letter === "Е"
     ) {
+      // Add class that will highlight vowels
       newSpan.classList.add("highlight");
     }
-
     // Making the <span> element show up
     divWithText.appendChild(newSpan);
   }
-
+  // Clear input form
   input.value = "";
 });
